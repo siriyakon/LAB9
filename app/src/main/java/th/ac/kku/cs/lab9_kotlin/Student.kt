e7c52ed48b63ceea30720d8477ca15034f27a76c
siriyakon <siriyakon.11326@gmail.com>
2018-11-12 17:22:16 +0700

    LAB9

---
+++ app/src/main/java/th/ac/kku/cs/lab9_kotlin/Student.kt
@@ -0,0 +1,34 @@
+package th.ac.kku.cs.lab9_kotlin
+
+import android.os.Parcel
+import android.os.Parcelable
+
+
+class Student (val id: String, val firstName: String, val lastname: String): Parcelable
+    {
+        constructor(parcel: Parcel) : this(
+                parcel.readString(),
+                parcel.readString(),
+                parcel.readString()) {
+        }
+
+        override fun writeToParcel(parcel: Parcel, flags: Int) {
+            parcel.writeString(id)
+            parcel.writeString(firstName)
+            parcel.writeString(lastname)
+        }
+
+        override fun describeContents(): Int {
+            return 0
+        }
+
+        companion object CREATOR : Parcelable.Creator<Student> {
+            override fun createFromParcel(parcel: Parcel): Student {
+                return Student(parcel)
+            }
+
+            override fun newArray(size: Int): Array<Student?> {
+                return arrayOfNulls(size)
+            }
+        }
+    }
